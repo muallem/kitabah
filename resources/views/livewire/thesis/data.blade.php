e1021e7afdef1665ddfadf9754461ce190818453
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-09 16:45:35 +0700

    fix

--- resources/views/livewire/thesis/data.blade.php
+++ resources/views/livewire/thesis/data.blade.php
@@ -1,21 +1,23 @@
-@if(!$thesis)
-    <button type="button" class="btn btn-success btn-icon-split mb-4" data-bs-toggle="modal" data-bs-target="#detailModal"
-        wire:click="resetInput">
-        Buat Judul Skripsi
-    </button>
-@else
-    <div class="alert alert-success" role="alert">
-        {{$thesis->title}}
-    </div>
-    @if($thesis->group === null)
-        <div class="alert alert-warning" role="alert">
-            Tunggu balasan dari admin!
-        </div>
+<div class="w-100">
+    @if(!$thesis)
+        <button type="button" class="btn btn-success btn-icon-split mb-4" data-bs-toggle="modal" data-bs-target="#detailModal"
+            wire:click="resetInput">
+            Buat Judul Skripsi
+        </button>
     @else
-        <div class="alert alert-info" role="alert">
-            {{$thesis->group}}
+        <div class="alert alert-success" role="alert">
+            {{$thesis->title}}
         </div>
-        
-        @livewire('discussion.detail')
+        @if($thesis->group === null)
+            <div class="alert alert-warning" role="alert">
+                Tunggu balasan dari admin!
+            </div>
+        @else
+            <div class="alert alert-info" role="alert">
+                {{$thesis->group}}
+            </div>
+            
+            @livewire('discussion.detail')
+        @endif
     @endif
-@endif
+</div>
