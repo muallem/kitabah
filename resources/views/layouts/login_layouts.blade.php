cc5a10b096ab523e8d0b98862187934be0dca442
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-09 14:21:57 +0700

    js

--- resources/views/layouts/login_layouts.blade.php
+++ resources/views/layouts/login_layouts.blade.php
@@ -22,6 +22,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.9/dist/sweetalert2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    @stack('js')
+    @yield('js')
 </body>
 </html>
