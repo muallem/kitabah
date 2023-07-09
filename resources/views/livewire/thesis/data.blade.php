af9ce253de143d7febf90baddfc8292b581f075f
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-09 16:24:41 +0700

    if

--- resources/views/livewire/thesis/data.blade.php
+++ resources/views/livewire/thesis/data.blade.php
@@ -1,10 +1,19 @@
 @if(!$thesis)
-<button type="button" class="btn btn-success btn-icon-split mb-4" data-bs-toggle="modal" data-bs-target="#detailModal"
-    wire:click="resetInput">
-    Buat Judul Skripsi
-</button>
+    <button type="button" class="btn btn-success btn-icon-split mb-4" data-bs-toggle="modal" data-bs-target="#detailModal"
+        wire:click="resetInput">
+        Buat Judul Skripsi
+    </button>
 @else
-<div class="alert alert-info" role="alert">
-    {{$thesis->title}}
-  </div>
-  @endif
+    <div class="alert alert-info" role="alert">
+        {{$thesis->title}}
+    </div>
+    @if(!$thesis->group)
+        <div class="alert alert-warning" role="alert">
+            Tunggu balasan dari admin!
+        </div>
+    @else
+        <div class="alert alert-warning" role="alert">
+            {{$thesis->group}}
+        </div>
+    @endif
+@endif
