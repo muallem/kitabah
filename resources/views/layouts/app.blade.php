49c3f8572fb7fe922cd924ae9404d9bb102e874c
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-15 12:41:50 +0700

    try

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,10 +105,10 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('logout') }}">Logout</a>
                         </li>
-                        <li class="sidebar2 d-none"><a href="#">Home</a></li>
-                        <li class="sidebar2 d-none"><a href="#">About</a></li>
-                        <li class="sidebar2 d-none"><a href="#">Services</a></li>
-                        <li class="sidebar2 d-none"><a href="#">Contact</a></li>
+                        <li class="sidebar2"><a href="#">Home</a></li>
+                        <li class="sidebar2"><a href="#">About</a></li>
+                        <li class="sidebar2"><a href="#">Services</a></li>
+                        <li class="sidebar2"><a href="#">Contact</a></li>
                     </ul>
                 </div>
             </div>
