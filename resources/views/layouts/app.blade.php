4fbebc7d0b2f7e5012848c7c7854ee9d1a1cdd82
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-15 13:05:16 +0700

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,61 +17,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.9/dist/sweetalert2.min.css">
 
     @livewireStyles
-    <style>
-
-    .navbar{
-        z-index: 999;
-    }
-         .sidebar {
-        position: fixed;
-        padding-top: 70px;
-         /* Adjust the value as needed to create the desired margin-top */
-        left: 0;
-        width: 200px;
-        height: 100%;
-        background-color: #f1f1f1;
-        z-index: 99; /* Ensure the sidebar appears above other elements */
-    }
-    .sidebar2{
-        display: none;
-    }
-    
-    .sidebar ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    
-    .sidebar ul li {
-        padding: 10px;
-    }
-    
-    .sidebar ul li a {
-        text-decoration: none;
-        color: #333;
-    }
-         /* Existing CSS styles for the sidebar */
-    
-    @media (max-width: 767px) {
-        /* Hide the sidebar and display a hamburger menu */
-        .sidebar {
-            display: none;
-        }
-        .sidebar2 {
-            display: block;
-        }
-        
-        /* Show the hamburger menu icon */
-        .hamburger-menu {
-            display: block;
-            position: fixed;
-            top: 10px;
-            left: 10px;
-            width: 30px;
-            height: 30px;
-            background-color: #f1f1f1;
-        }
-    }
-    </style>
+  
     
 @stack('css')
 </head>
--- resources/views/livewire/admin-thesis/sidebar.blade.php
+++ resources/views/livewire/admin-thesis/sidebar.blade.php
@@ -1,4 +1,61 @@
 
+@push('css')
+<style>
+
+    .navbar{
+        z-index: 999;
+    }
+         .sidebar {
+        position: fixed;
+        padding-top: 70px;
+         /* Adjust the value as needed to create the desired margin-top */
+        left: 0;
+        width: 200px;
+        height: 100%;
+        background-color: #f1f1f1;
+        z-index: 99; /* Ensure the sidebar appears above other elements */
+    }
+    .sidebar2{
+        display: none;
+    }
+    
+    .sidebar ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    
+    .sidebar ul li {
+        padding: 10px;
+    }
+    
+    .sidebar ul li a {
+        text-decoration: none;
+        color: #333;
+    }
+         /* Existing CSS styles for the sidebar */
+    
+    @media (max-width: 767px) {
+        /* Hide the sidebar and display a hamburger menu */
+        .sidebar {
+            display: none;
+        }
+        .sidebar2 {
+            display: block;
+        }
+        
+        /* Show the hamburger menu icon */
+        .hamburger-menu {
+            display: block;
+            position: fixed;
+            top: 10px;
+            left: 10px;
+            width: 30px;
+            height: 30px;
+            background-color: #f1f1f1;
+        }
+    }
+    </style>
+@endpush
 <ul class="navbar-nav ms-auto">
     @foreach($thesis as $item)
         <li class="nav-item"><a href="#">{{$item->user->user_login}}</a></li>
