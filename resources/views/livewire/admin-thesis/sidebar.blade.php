d09fdce71701bf1f25cc6fce659568b9c4d8ab97
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-15 13:35:37 +0700

    hmms

--- resources/views/livewire/admin-thesis/sidebar.blade.php
+++ resources/views/livewire/admin-thesis/sidebar.blade.php
@@ -59,7 +59,7 @@
 <ul class="navbar-nav ms-auto">
     @foreach($thesis as $item)
     <li class="nav-item">
-        <a href="#" data-bs-toggle="modal" data-bs-target="#modalDiscussion" wire:click="$emit('editDetailThesis', {{ $item->id }})" class="btn btn-info">{{ $item->user->user_login }}</a>
+        <a href="#" data-bs-toggle="modal" data-bs-target="#modalDiscussion" wire:click="$emit('showDiscussion', {{ $item->id }})" class="btn btn-info">{{ $item->user->user_login }}</a>
     </li>
     
     @endforeach
@@ -68,7 +68,7 @@
     
 <ul class="navbar-nav ms-auto">
     @foreach($thesis as $item)
-        <li class="nav-item sidebar2"><button type="button" data-bs-toggle="modal" data-bs-target="#modalDiscussion" class="btn btn-info text-decoration-none text-dark">{{$item->user->user_login}}</a></li>
+        <li class="nav-item sidebar2"><button type="button" data-bs-toggle="modal" data-bs-target="#modalDiscussion"  wire:click="$emit('showDiscussion', {{ $item->id }})" class="btn btn-info text-decoration-none text-dark">{{$item->user->user_login}}</a></li>
     @endforeach
 </ul>
 @endpush
