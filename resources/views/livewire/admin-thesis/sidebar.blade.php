1e84cca2c8c703a78fcddbabdf6ba84984f85647
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-15 13:03:33 +0700

    hmm

--- resources/views/livewire/admin-thesis/sidebar.blade.php
+++ resources/views/livewire/admin-thesis/sidebar.blade.php
@@ -1,6 +1,6 @@
 
 <ul class="navbar-nav ms-auto">
     @foreach($thesis as $item)
-        <li class="nav-item"><a href="#">{{$thesis->user->user_login}}</a></li>
+        <li class="nav-item"><a href="#">{{$item->user->user_login}}</a></li>
     @endforeach
 </ul>
