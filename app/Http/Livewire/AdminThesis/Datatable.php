1ef485164f95d2a7b2f7783c8426da98b6a619f0
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-16 17:18:12 +0700

    p

--- app/Http/Livewire/AdminThesis/Datatable.php
+++ app/Http/Livewire/AdminThesis/Datatable.php
@@ -106,7 +106,9 @@ class Datatable extends Component
 
     public function getQuery(): Builder
     {
-        return Thesis::select('id', 'title', 'group', 'student_id')->with('wpjs_users');
+        return Thesis::select('id', 'title', 'group', 'student_id')->with(['wpjs_users' => function($query){
+            $query->select('ID', 'user_login');
+        }]);
     }
 
     public function getView(): String
