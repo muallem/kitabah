05b97c3dae20de73db659fbcdab5c52b29ac2dc7
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-19 19:14:05 +0700

    update_last_seen

--- app/Http/Livewire/Discussion/Chat.php
+++ app/Http/Livewire/Discussion/Chat.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire\Discussion;
 
 use Livewire\Component;
 use App\Models\Discussion;
+use App\Models\Thesis;
+use Carbon\Carbon;
 
 class Chat extends Component
 {
@@ -21,17 +23,32 @@ class Chat extends Component
     {
         $this->theses_id = $id;
         $this->discussions = Discussion::where('theses_id', $id)->get();
+        if (session()->get('user_role') === 'admin'){
+            $theses = Thesis::where('id', $id)->first();
+            $theses->last_seen = Carbon::now();
+            $theses->save();
+        }
     }
 
     public function showChat($id){
         $this->theses_id = $id;
         $this->discussions = Discussion::where('theses_id', $id)->get();
+        if (session()->get('user_role') === 'admin'){
+            $theses = Thesis::where('id', $id)->first();
+            $theses->last_seen = Carbon::now();
+            $theses->save();
+        }
         return view('livewire.discussion.chat');
     }
 
     public function render()
     {
         $this->discussions = Discussion::where('theses_id', $this->theses_id)->get();
+        if (session()->get('user_role') === 'admin'){
+            $theses = Thesis::where('id', $this->theses_id)->first();
+            $theses->last_seen = Carbon::now();
+            $theses->save();
+        }
         return view('livewire.discussion.chat');
     }
 
