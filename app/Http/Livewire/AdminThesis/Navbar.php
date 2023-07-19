374663c233e68f43abc53edbd41b3d907f51e315
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-19 19:01:31 +0700

    fix

--- app/Http/Livewire/AdminThesis/Navbar.php
+++ app/Http/Livewire/AdminThesis/Navbar.php
@@ -19,12 +19,13 @@ class Navbar extends Component
     public function mount()
     {
         // $this->thesis = Thesis::select('id', 'student_id')->with('user')->get();
-        $this->thesis = Thesis::select('theses.*', DB::raw('COUNT(discussions.id) as discussion_count'))
+        $this->thesis = Thesis::select('theses.id', 'users.user_login', DB::raw('COUNT(discussions.id) as discussion_count'))
         ->leftJoin('discussions', function ($join) {
             $join->on('theses.id', '=', 'discussions.theses_id')
                 ->where('discussions.created_at', '>', DB::raw('theses.last_seen'));
         })
-        ->groupBy('theses.id')
+        ->leftJoin('users', 'theses.student_id', '=', 'users.id')
+        ->groupBy('theses.id', 'users.user_login')
         ->get();
     }
     
