1129aaf67c892604a2af3c1e7ec48f019ff3f2ce
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-19 19:02:22 +0700

    p

--- app/Http/Livewire/AdminThesis/Navbar.php
+++ app/Http/Livewire/AdminThesis/Navbar.php
@@ -19,26 +19,26 @@ class Navbar extends Component
     public function mount()
     {
         // $this->thesis = Thesis::select('id', 'student_id')->with('user')->get();
-        $this->thesis = Thesis::select('theses.id', 'users.user_login', DB::raw('COUNT(discussions.id) as discussion_count'))
+        $this->thesis = Thesis::select('theses.id', 'wpjs_users.user_login', DB::raw('COUNT(discussions.id) as discussion_count'))
         ->leftJoin('discussions', function ($join) {
             $join->on('theses.id', '=', 'discussions.theses_id')
                 ->where('discussions.created_at', '>', DB::raw('theses.last_seen'));
         })
-        ->leftJoin('users', 'theses.student_id', '=', 'users.id')
-        ->groupBy('theses.id', 'users.user_login')
+        ->leftJoin('wpjs_users', 'theses.student_id', '=', 'wpjs_users.id')
+        ->groupBy('theses.id', 'wpjs_users.user_login')
         ->get();
     }
     
     public function render()
     {
         // $this->thesis = Thesis::select('id', 'student_id')->with('user')->get();
-        $this->thesis = Thesis::select('theses.id', 'users.user_login', DB::raw('COUNT(discussions.id) as discussion_count'))
+        $this->thesis = Thesis::select('theses.id', 'wpjs_users.user_login', DB::raw('COUNT(discussions.id) as discussion_count'))
         ->leftJoin('discussions', function ($join) {
             $join->on('theses.id', '=', 'discussions.theses_id')
                 ->where('discussions.created_at', '>', DB::raw('theses.last_seen'));
         })
-        ->leftJoin('users', 'theses.student_id', '=', 'users.id')
-        ->groupBy('theses.id', 'users.user_login')
+        ->leftJoin('wpjs_users', 'theses.student_id', '=', 'wpjs_users.id')
+        ->groupBy('theses.id', 'wpjs_users.user_login')
         ->get();
     
         return view('livewire.admin-thesis.navbar');
