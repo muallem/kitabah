dbd539e0ff809412818f681852b4a80b49c44fb9
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-19 21:09:51 +0700

    y

--- resources/views/livewire/thesis/data.blade.php
+++ resources/views/livewire/thesis/data.blade.php
@@ -5,31 +5,33 @@
             Buat Judul Skripsi
         </button>
     @else
-    <div class="row">
-        <div class="col-9">
-            <div class="alert alert-success" role="alert">
-                {{$thesis->title}}
-            </div>
-        </div>
-        <div class="col-3">
-            <a href="{{ route('print', ['id' => Crypt::encrypt($thesis->id)]) }}" target="_blank" class="btn btn-outline-info btn-block"><i class="fas fa-print mr-2"></i>Cetak</a>
-        </div>
+    <div class="alert alert-success" role="alert">
+        {{$thesis->title}}
     </div>
+    
     
     @if($thesis->group === null)
             <div class="alert alert-warning" role="alert">
                 Tunggu balasan dari admin!
             </div>
         @else
-            <div class="alert alert-info" role="alert">
-                @if($thesis->group == 'r&d')
-                R&D (Research and Development)
-                @elseif($thesis->group == "kuantitas")
-                Kuantitas (kuan)
-                @elseif($thesis->group == "kualitas")
-                Kualitas (kual)
-                @endif
+            <div class="row">
+                <div class="col-9">
+                    <div class="alert alert-info" role="alert">
+                        @if($thesis->group == 'r&d')
+                        R&D (Research and Development)
+                        @elseif($thesis->group == "kuantitas")
+                        Kuantitas (kuan)
+                        @elseif($thesis->group == "kualitas")
+                        Kualitas (kual)
+                        @endif
+                    </div>
+                </div>
+                <div class="col-3">
+                    <a href="{{ route('print', ['id' => Crypt::encrypt($thesis->id)]) }}" target="_blank" class="btn btn-outline-info btn-block"><i class="fas fa-print mr-2"></i>Cetak</a>
+                </div>
             </div>
+            
             @livewire('discussion.detail', ['id' => $thesis->id])
         @endif
     @endif
