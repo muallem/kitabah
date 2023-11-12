7925f80a95d08a273ddb5cf38d22232f0310dea9
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-11-12 12:22:08 +0700

    i

--- resources/views/materi/kual.blade.php
+++ resources/views/materi/kual.blade.php
@@ -26,39 +26,52 @@
               </ul>
               <div class="tab-content" id="myTabContent">
                   <div class="tab-pane fade show active" id="pendahuluan-tab-pane" role="tabpanel" aria-labelledby="pendahuluan-tab" tabindex="0">
-                    <ul class="nav nav-tabs" id="myTab" role="tablist">
-                        <li class="nav-item" role="presentation">
-                          <button class="nav-link active" id="pendahuluan1-tab" data-bs-toggle="tab" data-bs-target="#pendahuluan1-tab-pane" type="button" role="tab" aria-controls="pendahuluan1-tab-pane" aria-selected="true">Pendahuluan 1</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                          <button class="nav-link" id="pendahuluan2-tab" data-bs-toggle="tab" data-bs-target="#pendahuluan2-tab-pane" type="button" role="tab" aria-controls="pendahuluan2-tab-pane" aria-selected="false">Pendahuluan 2</button>
-                        </li>
-                      </ul>
-                      <div class="tab-content" id="myTabContent">
-                        <div class="tab-pane fade show active" id="pendahuluan1-tab-pane" role="tabpanel" aria-labelledby="pendahuluan1-tab" tabindex="0">
-                          <h1>Pendahuluan 1</h1>
-                          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est debitis nam aliquid totam? Ratione neque similique veniam, eos fuga accusantium! Dolor blanditiis, hic et magni saepe cum reiciendis commodi quaerat asperiores sit itaque facilis voluptatum quibusdam ea amet nisi mollitia!</p>
-                      </div>
-                        <div class="tab-pane fade" id="pendahuluan2-tab-pane" role="tabpanel" aria-labelledby="pendahuluan2-tab" tabindex="0">
-                          <h1>Pendahuluan 2</h1>
-                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est debitis nam aliquid totam? Ratione neque similique veniam, eos fuga accusantium! Dolor blanditiis, hic et magni saepe cum reiciendis commodi quaerat asperiores sit itaque facilis voluptatum quibusdam ea amet nisi mollitia!</p>
-                        </div>
-                      </div>
+                    <h1>Pendahuluan</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est debitis nam aliquid totam? Ratione neque similique veniam, eos fuga accusantium! Dolor blanditiis, hic et magni saepe cum reiciendis commodi quaerat asperiores sit itaque facilis voluptatum quibusdam ea amet nisi mollitia!</p>
+                   
                     
                 </div>
                 <div class="tab-pane fade" id="bab1-tab-pane" role="tabpanel" aria-labelledby="bab1-tab" tabindex="0">
+                    <h1>
+                        Bab 1
+                    </h1>
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                          <button class="nav-link active" id="bab1-sub1-tab" data-bs-toggle="tab" data-bs-target="#bab1-sub1-tab-pane" type="button" role="tab" aria-controls="bab1-sub1-tab-pane" aria-selected="true">Sub 1</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                          <button class="nav-link" id="bab1-sub1-tab" data-bs-toggle="tab" data-bs-target="#bab1-sub1-tab-pane" type="button" role="tab" aria-controls="bab1-sub1-tab-pane" aria-selected="false">Sub 2</button>
+                        </li>
+                      </ul>
+                      <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="bab1-sub1-tab-pane" role="tabpanel" aria-labelledby="bab1-sub1-tab" tabindex="0">
+                          <h1>Sub 1</h1>
+                          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est debitis nam aliquid totam? Ratione neque similique veniam, eos fuga accusantium! Dolor blanditiis, hic et magni saepe cum reiciendis commodi quaerat asperiores sit itaque facilis voluptatum quibusdam ea amet nisi mollitia!</p>
+                        </div>
+                        <div class="tab-pane fade" id="bab1-sub1-tab-pane" role="tabpanel" aria-labelledby="bab1-sub1-tab" tabindex="0">
+                          <h1>Sub 2</h1>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est debitis nam aliquid totam? Ratione neque similique veniam, eos fuga accusantium! Dolor blanditiis, hic et magni saepe cum reiciendis commodi quaerat asperiores sit itaque facilis voluptatum quibusdam ea amet nisi mollitia!</p>
+                        </div>
+                      </div>
                     <h1>Bab 1</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloribus, reiciendis sapiente esse omnis id debitis commodi, velit ex, corrupti vitae? Nesciunt quaerat, dolor error quam eum quidem velit unde laboriosam mollitia cumque praesentium a obcaecati expedita aut id amet!</p>
-
-                    <div class="mb-3">
-                        <label for="formFileMultiple" class="form-label">Multiple files input example</label>
-                        <input class="form-control" type="file" id="formFileMultiple" multiple>
+                    <div class="row">
+                        <div class="col-mb-4">
+                            <label for="formFileMultiple" class="form-label">Upload File</label>
+                            <input class="form-control" type="file" id="formFileMultiple" multiple>
+                        </div>
                     </div>
   
                 </div>
                 <div class="tab-pane fade" id="bab2-tab-pane" role="tabpanel" aria-labelledby="bab2-tab" tabindex="0">
                     <h1>Bab 2</h1>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta debitis, reprehenderit non obcaecati maxime dolor reiciendis facere, odit dignissimos, quidem expedita placeat? Neque similique accusamus consequuntur alias officia nihil fugiat vero velit eligendi cupiditate laborum a, pariatur eveniet veritatis quaerat!</p>
+                    <div class="row">
+                        <div class="col-mb-4">
+                            <label for="formFileMultiple" class="form-label">Upload File</label>
+                            <input class="form-control" type="file" id="formFileMultiple" multiple>
+                        </div>
+                    </div>
                 </div>
               </div>
         </div>
