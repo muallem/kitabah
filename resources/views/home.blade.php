ce158c9222aac3b10fb81b81cfbab283f7474aa8
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-11-13 15:37:55 +0700

    p

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -604,10 +604,10 @@
     </div>
 
     <!-- Javascript -->
-    <script src="assets/bundles/libscripts.bundle.js"></script>
-    <script src="assets/bundles/vendorscripts.bundle.js"></script>
+    <script src="{{asset('assets/bundles/libscripts.bundle.js')}}"></script>
+    <script src="{{asset('assets/bundles/vendorscripts.bundle.js')}}"></script>
 
-    <script src="assets/bundles/mainscripts.bundle.js"></script>
+    <script src="{{asset('assets/bundles/mainscripts.bundle.js')}}"></script>
     <script>
         $('.metismenu li').click(function () {
             var $this = $(this);
