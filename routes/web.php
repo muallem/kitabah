0c42ef1581efd1df5956f8b5d77cb6ce77c89b77
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-11-13 15:31:22 +0700

    louout

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,19 @@ Route::group(['middleware' => 'guest'], function () {
     Route::get('/login', [AuthController::class, 'index'])->name('login.index');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
+Route::get('coba', [HomeController::class, 'coba']);
+Route::get('kual', [HomeController::class, 'kual'])->name('kual');
+Route::get('/print/{id}', [App\Http\Controllers\DiscussionController::class, 'print'])->name('print');
+Route::group(['middleware' => ['my.auth', 'admin']], function () {
+    Route::get('/home', [App\Http\Controllers\ThesisController::class, 'admin_thesis'])->name('home');
+    Route::get('/admin_thesis', [App\Http\Controllers\ThesisController::class, 'admin_thesis'])->name('thesis.admin_thesis');
+    Route::get('/discussion', [App\Http\Controllers\DiscussionController::class, 'index'])->name('thesis.discussion');
+});
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::group(['middleware' => ['my.auth', 'student']], function () {
+    Route::get('/home', [App\Http\Controllers\ThesisController::class, 'index'])->name('home');
+    Route::get('/thesis', [App\Http\Controllers\ThesisController::class, 'index'])->name('thesis.index');
+    Route::get('/discussion', [App\Http\Controllers\DiscussionController::class, 'index'])->name('thesis.discussion');
+});
+Route::get('get_session', [HomeController::class, 'get_session']);
+Route::get('coba', [HomeController::class, 'coba']);
