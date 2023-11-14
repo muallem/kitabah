539e172372c11dab05e501d1255847e88cc4867f
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-11-14 18:49:35 +0700

    coba

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -25,13 +25,12 @@ class AdminController extends Controller
      */
     public function index()
     {
-        $data = Judul::select('juduls.id', 'wpjs_users.user_login', DB::raw('COUNT(materis.id) as materi_count'), DB::raw('juduls.group'))
+        $data = Judul::select('juduls.id', 'juduls.group', 'wpjs_users.user_login', DB::raw('COUNT(materis.id) as materi_count'))
         ->leftJoin('materis', function ($join) {
             $join->on('juduls.student_id', '=', 'materis.student_id')
                 ->where('materis.created_at', '>', DB::raw('juduls.last_seen'));
         })
         ->leftJoin('wpjs_users', 'juduls.student_id', '=', 'wpjs_users.id')
-        ->groupBy('juduls.id', 'wpjs_users.user_login')
         ->get();
         return $data;
         return view('admin.index');
