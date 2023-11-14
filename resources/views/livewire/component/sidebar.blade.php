208a5dda968341ae19b6729577caecdbe0db89cf
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-11-14 19:19:47 +0700

    null

--- resources/views/livewire/component/sidebar.blade.php
+++ resources/views/livewire/component/sidebar.blade.php
@@ -29,7 +29,7 @@
                     <ul class="metismenu list-unstyled">
                         @foreach($thesis as $item)
                             <li>
-                                <a href="#" class="has-arrow text-decoration-none text-dark p-1" aria-expanded="false">
+                                <a href="#" class="has-arrow text-decoration-none text-dark" aria-expanded="false">
                                     <i class="fa fa-briefcase"></i>
                                     <span>{{$item->user_login}}</span> 
                                     {!! ($item->group) ? '' : '<i class="fa fa-exclamation-circle"></i>' ; !!}
