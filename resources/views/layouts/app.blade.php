2547e64c2fa9e0eac7a2ba222bad7d52df1a24a0
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-11-14 17:12:00 +0700

    gj

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,6 +111,88 @@
                                 </ul>
                             </nav>
                         </div>
+                        
+                <div class="tab-pane fade" id="setting_menu" role="tabpanel" >
+                    <div class="px-3">
+                        <h6>Choose Skin</h6>
+                        <ul class="choose-skin list-unstyled">
+                            <li data-theme="purple" class="mb-2"><div class="purple"></div><span>Purple</span></li>
+                            <li data-theme="blue" class="mb-2"><div class="blue"></div><span>Blue</span></li>
+                            <li data-theme="cyan" class="active mb-2"><div class="cyan"></div><span>Cyan</span></li>
+                            <li data-theme="green" class="mb-2"><div class="green"></div><span>Green</span></li>
+                            <li data-theme="orange" class="mb-2"><div class="orange"></div><span>Orange</span></li>
+                            <li data-theme="blush" class="mb-2"><div class="blush"></div><span>Blush</span></li>
+                        </ul>
+                        <hr>
+                        <h6>Theme Option</h6>
+                        <ul class="list-unstyled">
+                            <li class="d-flex align-items-center mb-1">
+                                <div class="form-check form-switch theme-switch">
+                                    <input class="form-check-input" type="checkbox" id="theme-switch">
+                                    <label class="form-check-label" for="theme-switch">Enable Dark Mode!</label>
+                                </div>
+                            </li>
+                            <li class="d-flex align-items-center mb-1">
+                                <div class="form-check form-switch theme-high-contrast">
+                                    <input class="form-check-input" type="checkbox" id="theme-high-contrast">
+                                    <label class="form-check-label" for="theme-high-contrast">Enable High Contrast</label>
+                                </div>
+                            </li>
+                            <li class="d-flex align-items-center mb-1">
+                                <div class="form-check form-switch theme-rtl">
+                                    <input class="form-check-input" type="checkbox" id="theme-rtl">
+                                    <label class="form-check-label" for="theme-rtl">Enable RTL Mode!</label>
+                                </div>
+                            </li>
+                            <li class="d-flex align-items-center mb-1">
+                                <div class="form-check form-switch minisidebar-active">
+                                    <input class="form-check-input" type="checkbox" id="mini-active">
+                                    <label class="form-check-label" for="mini-active">Mini Sidebar</label>
+                                </div>
+                            </li>
+                        </ul>
+                        <hr>
+                        <h6>General Settings</h6>
+                        <ul class="setting-list list-unstyled">
+                            <li>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                    <label class="form-check-label" for="flexCheckDefault">Default checkbox</label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1">
+                                    <label class="form-check-label" for="flexCheckDefault1">Email Redirect</label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault2" checked>
+                                    <label class="form-check-label" for="flexCheckDefault2">Notifications</label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault3">
+                                    <label class="form-check-label" for="flexCheckDefault3">Auto Updates</label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault4">
+                                    <label class="form-check-label" for="flexCheckDefault4">Offline</label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault5">
+                                    <label class="form-check-label" for="flexCheckDefault5">Location Permission</label>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
                     </div>
                 </div>
         
@@ -131,6 +213,7 @@
         @livewireScripts
 
         <script>
+            
             window.livewire.on('onSuccessSweetAlert', (message) => {
                 Swal.fire({
                     icon: 'success',
