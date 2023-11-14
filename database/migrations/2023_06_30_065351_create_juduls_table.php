7f31114f722ca86601a43d41d5ede209b4dcab19
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-11-14 15:49:03 +0700

    gj

--- database/migrations/2023_06_30_065351_create_juduls_table.php
+++ database/migrations/2023_06_30_065351_create_juduls_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('judul', function (Blueprint $table) {
+        Schema::create('juduls', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('group')->nullable()->default(null);
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('theses');
+        Schema::dropIfExists('juduls');
     }
 };
