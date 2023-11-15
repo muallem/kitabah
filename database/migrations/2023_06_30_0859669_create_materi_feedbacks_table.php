7b681f527b38a0a60f7ab4d97150d3188cc5cf9c
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-11-15 19:15:03 +0700

    migrate

--- database/migrations/2023_06_30_0859669_create_materi_feedbacks_table.php
+++ database/migrations/2023_06_30_0859669_create_materi_feedbacks_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('materi_feedbacks', function (Blueprint $table) {
+        Schema::create('materi_feedback', function (Blueprint $table) {
             $table->id();
             $table->text('feedback');
             $table->text('kode_materi');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('materi_feedbacks');
+        Schema::dropIfExists('materi_feedback');
     }
 };
