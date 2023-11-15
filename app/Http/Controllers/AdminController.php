9cd2f16deea5dd7dd493e900408a998059e22174
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-11-15 19:48:06 +0700

    y

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -38,13 +38,13 @@ class AdminController extends Controller
     public function rnd()
     {
 
-        $thesis = Judul::select('juduls.id', 'juduls.group', 'wpjs_users.user_login', 'materi_feedback.feedback', DB::raw('COUNT(materi_feedback.id) as materi_count'))
+        $thesis = Judul::select('juduls.id', 'juduls.group', 'wpjs_users.user_login', DB::raw('COUNT(materi_feedback.id) as materi_count'))
         ->leftJoin('materi_feedback', function ($join) {
             $join->on('juduls.student_id', '=', 'materi_feedback.student_id')
                 ->whereNull('materi_feedback.feedback');
         })
         ->leftJoin('wpjs_users', 'juduls.student_id', '=', 'wpjs_users.id')
-        ->groupBy('juduls.id', 'juduls.group', 'wpjs_users.user_login', 'materi_feedback.feedback')
+        ->groupBy('juduls.id', 'juduls.group', 'wpjs_users.user_login')
         ->get();
 
         return $thesis;
