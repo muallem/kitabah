cd847e5fb3965c5d9d9eff6e3adae9fb30bf1260
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-11-15 19:18:55 +0700

    cas

--- app/Http/Livewire/Component/Sidebar.php
+++ app/Http/Livewire/Component/Sidebar.php
@@ -18,8 +18,7 @@ class Sidebar extends Component
         {
             $this->thesis = Judul::select('juduls.id', 'juduls.group', 'wpjs_users.user_login', DB::raw('COUNT(materis.id) as materi_count'))
             ->leftJoin('materis', function ($join) {
-                $join->on('juduls.student_id', '=', 'materis.student_id')
-                    ->where('materis.admin_feedback', "");
+                $join->on('juduls.student_id', '=', 'materis.student_id');
             })
             ->leftJoin('wpjs_users', 'juduls.student_id', '=', 'wpjs_users.id')
             ->groupBy('juduls.id', 'juduls.group', 'wpjs_users.user_login')
