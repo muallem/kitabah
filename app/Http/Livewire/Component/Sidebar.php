249945400432488d15301620a034b95aff5255b9
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-11-16 15:25:20 +0700

    p

--- app/Http/Livewire/Component/Sidebar.php
+++ app/Http/Livewire/Component/Sidebar.php
@@ -29,14 +29,15 @@ class Sidebar extends Component
     {
         if(AuthHelper::isAdmin())
         {
-            $this->thesis = Judul::select('juduls.id', 'juduls.group', 'wpjs_users.user_login', DB::raw('COUNT(materi_feedback.id) as materi_count'))
+            $thesis = Judul::select('juduls.id', 'juduls.group', 'wpjs_users.user_login', DB::raw('COUNT(materi_feedback.id) as materi_count'))
             ->leftJoin('materi_feedback', function ($join) {
                 $join->on('juduls.student_id', '=', 'materi_feedback.student_id')
                     ->whereNull('materi_feedback.feedback');
             })
             ->leftJoin('wpjs_users', 'juduls.student_id', '=', 'wpjs_users.id')
             ->groupBy('juduls.id', 'juduls.group', 'wpjs_users.user_login')
-            ->get();
+            ->get()->toArray();
+            $this->thesis = $thesis;
         }else{
             $this->thesis = Judul::where('student_id', session()->get('user_id'))->first();
         }
